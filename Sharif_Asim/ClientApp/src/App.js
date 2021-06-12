1bc2dd1b60e5983b7b1ab9c1dac3c3c1823f64ae
asra <assem.rh@outlook.com>
2021-06-12 11:40:32 +0300

    Edit contents files

--- Sharif_Asim/ClientApp/src/App.js
+++ Sharif_Asim/ClientApp/src/App.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
+import { Route, Switch, useLocation } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+//import { NotFound } from './components/NotFound';
 
 import './custom.css'
+
+
+
+
 
 export default class App extends Component {
   static displayName = App.name;
 
   render () {
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
+        <Layout>
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route path='/counter' component={Counter} />
+                <Route path='/fetch-data' component={FetchData} />
+            {/*    <Route path='*' component={NotFound} />*/}
+            </Switch>
       </Layout>
     );
   }
---
+++ Sharif_Asim/ClientApp/src/components/NotFound.js
@@ -0,0 +1,22 @@
+﻿////import React, { Component } from 'react';
+////import { Container } from 'reactstrap';
+////import { Route, Switch, useLocation } from 'react-router';
+
+
+//////const NotFound = () => {
+//////    const { pathname } = useLocation()
+
+//////    return (
+//////            <div>
+//////                <Container>
+//////                    <h3>
+//////                    No match for <code>{pathname}</code> ...
+//////                    </h3>
+//////                </Container>
+//////            </div>
+//////        )
+//////}
+
+//////export default NotFound
+
+
